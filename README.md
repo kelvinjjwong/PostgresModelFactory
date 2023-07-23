3146b9060f0673cb393aec9e2721ff3636943769
Kelvin Wong <kelvinjjwong@outlook.com>
2023-07-23 22:57:53 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PostgresModelFactory
+
+A description of this package.
---
+++ Sources/PostgresModelFactory/PostgresModelFactory.swift
@@ -0,0 +1,6 @@
+public struct PostgresModelFactory {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
